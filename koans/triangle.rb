432aa528e46029bea29f24086caf9c07604bf518
Piotr Milewski <milewski.piotr@gmail.com>
2012-04-14 22:25:33 +0200

    about_triangle_project_2.rb - done

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -14,6 +14,10 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
+  sort_args = [a,b,c].sort
+  raise TriangleError if  sort_args[0] < 1 || sort_args[2] >=  sort_args[0] + sort_args[1] &&
+                                              sort_args[0] == sort_args[1] &&
+                                              sort_args[0] != sort_args[2]
   return :equilateral if a == b && a == c
   return :isosceles   if a == b || a == c || b == c
   return :scalene     if a != b && a != c
@@ -22,3 +26,15 @@ end
 # Error class used in part 2.  No need to change this code.
 class TriangleError < StandardError
 end
+
+
+  # The first assignment did not talk about how to handle errors.
+  # Let's handle that part now.
+#  def test_illegal_triangles_throw_exceptions
+#    assert_raise(TriangleError) do triangle(0, 0, 0) end
+#    assert_raise(TriangleError) do triangle(3, 4, -5) end
+#    assert_raise(TriangleError) do triangle(1, 1, 3) end
+#    assert_raise(TriangleError) do triangle(2, 4, 2) end
+    # HINT: for tips, see http://stackoverflow.com/questions/3834203/ruby-koan-151-raising-exceptions
+# end
+
